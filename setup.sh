a54703d411154e2c23680df5ca038f196713673e
mdan16 <t.t.mina16@gmail.com>
2021-08-21 11:21:25 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-cd $(dirname $0)
+#! /bin/bash
 
-cp nginx.conf /etc/nginx/nginx.conf
-systemctl reload nginx
+cd $(dirname $0)/go
+
+echo "Build go file"
+export PATH=/home/isucon/local/go/bin:/home/isucon/go/bin:$PATH
+go build
+
+cd ..
+
+if [[ $(hostname) = *"11" ]]; then
+    echo "Reload nginx config"
+    cp nginx.conf /etc/nginx/nginx.conf
+    systemctl reload nginx
+fi
